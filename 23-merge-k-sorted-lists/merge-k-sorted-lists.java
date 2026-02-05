cf98ca00c749b52bd048afbcbe6ddfd363ef15d4
Kuswanth05 <vtu26018@veltech.edu.in>
2026-02-05 10:21:30 +0530

    Time: 5 ms (42.91%) | Memory: 47.1 MB (11.70%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        
+        // Add all non-null heads to min-heap
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+        
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        
+        // Extract min repeatedly
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            current.next = minNode;
+            current = current.next;
+            
+            if (minNode.next != null) {
+                pq.offer(minNode.next);
+            }
+        }
+        
+        return dummy.next;
+    }
+}
