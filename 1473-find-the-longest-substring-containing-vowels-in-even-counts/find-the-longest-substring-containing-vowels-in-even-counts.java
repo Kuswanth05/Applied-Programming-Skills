7a21a6dc6cb8fa06fd33ff20a2868916373b9eb3
Kuswanth05 <vtu26018@veltech.edu.in>
2026-02-16 10:28:20 +0530

    Time: 46 ms (44.37%) | Memory: 47.3 MB (95.18%) - LeetSync

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int mask = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == 'a') mask ^= (1 << 0);
+            else if (c == 'e') mask ^= (1 << 1);
+            else if (c == 'i') mask ^= (1 << 2);
+            else if (c == 'o') mask ^= (1 << 3);
+            else if (c == 'u') mask ^= (1 << 4);
+
+            if (map.containsKey(mask)) {
+                maxLen = Math.max(maxLen, i - map.get(mask));
+            } else {
+                map.put(mask, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
